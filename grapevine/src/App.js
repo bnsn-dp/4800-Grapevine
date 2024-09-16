5eb99cda13b281b72c6b7433f451a7f7a898bd9b
mayela101 <mancheta@cpp.edu>
2024-09-15 20:58:10 -0700

    Sidebar Changes + Search Bar

--- grapevine/src/App.js
+++ grapevine/src/App.js
@@ -1,23 +1,50 @@
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+
+  // Adding Search Bar Component
+  const[searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <div className="App">
+
       <header className="App-header">
         <h1 className="App-title">Grapevine</h1>
       </header>
+
       <div className="App-content">
         <aside className="App-sidebar">
           <nav>
             <ul>
+
+              <h2 className="Sidebar-title">The Winery</h2>
+
               <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Contact</a></li>
+              <li><a href="#">Messages</a></li>
+              <li><a href="#">Communities</a></li>
+              <li><a href="#">Settings</a></li>
+
             </ul>
           </nav>
         </aside>
+
         <main className="App-main">
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="search-input"
+            />
+          </div>
           <p>Welcome to Grapevine!</p>
         </main>
       </div>
