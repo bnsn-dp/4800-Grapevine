4346e8320209972198735b26a9cf728a164dd950
Nickolas-Duong <nduong@cpp.edu>
2024-10-03 00:19:55 -0700

    Remove API folder

--- middleware-root/API/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('users', UserViewset, basename='users')
-urlpatterns = router.urls
