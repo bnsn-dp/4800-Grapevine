bb325a68ccc3a2f5e0242fad0b8faaa49a661acb
Nickolas-Duong <nduong@cpp.edu>
2024-10-02 23:36:48 -0700

    update urls of Middleware

--- middleware-root/Middleware/urls.py
+++ middleware-root/Middleware/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from api import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
-]
+
+]+urls.urlpatterns
