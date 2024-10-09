11be94bcd756d1d3291b3e74e8dfd5cfb2b4338d
Nickolas-Duong <nduong@cpp.edu>
2024-10-09 11:13:01 -0700

    Update urls.py
    add new table and functions to urlpatterns

--- middleware-root/api/urls.py
+++ middleware-root/api/urls.py
@@ -5,8 +5,12 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('users', UserViewset, basename='users')
 router.register('posts', PostViewset, basename='posts')
+router.register('createdposts', CreatedPostsViewset, basename='createdposts')
 
 urlpatterns = [
     path('api/getuserid/', getUserID, name='getuserid'),
     path('api/login/', login_user, name='login'),
+    path('api/getpostid/', getPostID, name='getpostid'),
+    path('api/getcreatedpostid/', getCreatedPostID, name='getcreatedpostid'),
+    path('api/get_user_posts/', get_user_posts, name='getuserposts'),
               ] + router.urls
