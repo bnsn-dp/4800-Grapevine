4b7c0ed6f9d7aa1e2d71e84e8a0cbbaada447757
Nickolas-Duong <nduong@cpp.edu>
2024-12-08 01:05:10 -0800

    Update engagement_view.py
    remove unused imports

--- middleware-root/api/Engagement/engagement_view.py
+++ middleware-root/api/Engagement/engagement_view.py
@@ -1,17 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import api_view
-from django.db.models import Q
-
 from .engagement_model import Engagement
 from .engagement_serializer import EngagementSerializer
 from rest_framework.response import Response
-from django.db import connection
-import json
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
 
 class EngagementViewset(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
