99510755c6f6efe0d1adee92194ffd0be0136654
Nickolas-Duong <nduong@cpp.edu>
2024-12-08 00:58:49 -0800

    Update display.jsx
    change heading and readd communities

--- frontend-root-fr/grapevine/src/functions/display.jsx
+++ frontend-root-fr/grapevine/src/functions/display.jsx
@@ -25,11 +25,11 @@ const GetSidebar = () => {
     <aside className="App-sidebar">
       <nav>
         <ul>
-          <h2 className="Sidebar-title">The Winery</h2>
+          <h2 className="Sidebar-title">Dashboard</h2>
           <NavItem href="/home" iconSrc="/icons/Home.png" altText="Home Icon" text="Home" />
           <NavItem href="/messages" iconSrc="/icons/Chat.png" altText="Chat Icon" text="Messages" />
-          <NavItem href="/Communities" iconSrc="/icons/Communities.png" altText="Community Icon" text="Communities" />
-          <NavItem href="/" iconSrc="/icons/Settings.png" altText="Setting Icon" text="Settings" />
+          <NavItem href="/Communities/overview" iconSrc="/icons/Communities.png" altText="Community Icon" text="Communities" />
+{/*           <NavItem href="/" iconSrc="/icons/Settings.png" altText="Setting Icon" text="Settings" /> */}
         </ul>
       </nav>
 
